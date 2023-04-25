e9a35ef1830cfa7c04eb332d08defebf6d40ce0e
xAdiro <adiro.rostek@gmail.com>
2023-04-25 22:52:12 +0200

    Added basic script for testing python execution

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,24 @@
-window.addEventListener("DOMContentLoaded", () => {
-  
+import { Command } from '@tauri-apps/api/shell';
+
+const tauriButton = document.querySelector('.tauri');
+
+tauriButton?.addEventListener('click', async () => {
+    console.log('Running python...');
+
+    const command = new Command('run-python', ['python/hello.py']);
+    command.on('close', (data) => {
+        console.log(
+            `command finished with code ${data.code} and signal ${data.signal}`,
+        );
+    });
+    command.on('error', (error) => console.error(`command error: "${error}"`));
+    command.stdout.on('data', (line) =>
+        console.log(`command stdout: "${line}"`),
+    );
+    command.stderr.on('data', (line) =>
+        console.log(`command stderr: "${line}"`),
+    );
+
+    const child = await command.spawn();
+    console.log('pid:', child.pid);
 });
