2fb271fdb9261239f33ce9cb614dcd6cac3bb45d
xAdiro <adiro.rostek@gmail.com>
2023-04-26 02:33:07 +0200

    Added basic displaying of infinite well

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,39 @@
 import { Command } from '@tauri-apps/api/shell';
+// import * as Plotly from 'plotly.js';
 
 const tauriButton = document.querySelector('.tauri');
 
 tauriButton?.addEventListener('click', async () => {
     console.log('Running python...');
 
-    const command = new Command('run-python', ['python/hello.py']);
-    command.on('close', (data) => {
-        console.log(
-            `command finished with code ${data.code} and signal ${data.signal}`,
-        );
+    const command = new Command('run-python', [
+        'python/infinite-well.py',
+        '1',
+        '2',
+    ]);
+
+    command.stdout.on('data', (line) => {
+        const xy: { x: Array<number>; y: Array<number> } = JSON.parse(line);
+        console.log(xy);
+        const trace = {
+            x: xy.x,
+            y: xy.y,
+            mode: 'lines',
+            type: 'scatter',
+        };
+
+        Plotly.newPlot('plotly-chart', [{ y: xy.y }]);
     });
-    command.on('error', (error) => console.error(`command error: "${error}"`));
-    command.stdout.on('data', (line) =>
-        console.log(`command stdout: "${line}"`),
-    );
-    command.stderr.on('data', (line) =>
-        console.log(`command stderr: "${line}"`),
-    );
+
+    // command.on('close', (data) => {
+    //     console.log(
+    //         `command finished with code ${data.code} and signal ${data.signal}`,
+    //     );
+    // });
+    // command.on('error', (error) => console.error(`command error: "${error}"`));
+    // command.stderr.on('data', (line) =>
+    //     console.log(`command stderr: "${line}"`),
+    // );
 
     const child = await command.spawn();
     console.log('pid:', child.pid);
