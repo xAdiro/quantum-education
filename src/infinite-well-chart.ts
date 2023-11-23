f1721dfaa68b9754b0a177c5d636d64c0cea442f
xAdiro <adiro.rostek@gmail.com>
2023-11-23 03:03:33 +0100

    removed python code execution from infinite well chart

--- src/infinite-well-chart.ts
+++ src/infinite-well-chart.ts
@@ -1,5 +1,4 @@
 import { draw, updateWellChart } from './line-chart';
-import { getInfiniteWell } from './commands';
 import { Chart } from 'chart.js';
 
 const slider = document.querySelector('#n-setting') as HTMLInputElement;
@@ -10,22 +9,13 @@ let chart: Chart;
 type plotData = { x: Array<number>; y: Array<number> };
 
 window.addEventListener('DOMContentLoaded', async () => {
-    const command = getInfiniteWell(a, BigInt(1));
-    command.stdout.on('data', (text) => {
-        const xy: plotData = JSON.parse(text);
-        chart = draw(xy, target, a);
-    });
-    await command.spawn();
+    const text = "";
+    const xy: plotData = JSON.parse(text);
+    chart = draw(xy, target, a);
 });
 
 slider?.addEventListener('input', async () => {
-    const n = slider.value;
-    const command = getInfiniteWell(a, BigInt(n));
-
-    command.stdout.on('data', (text) => {
-        const xy: plotData = JSON.parse(text);
-
-        updateWellChart(chart, xy);
-    });
-    await command.spawn();
+    const text = "";
+    const xy: plotData = JSON.parse(text);
+    updateWellChart(chart, xy);
 });
