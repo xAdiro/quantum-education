20631a9f6ece01c37ee5b778be925f42923b16ec
xAdiro <adiro.rostek@gmail.com>
2023-12-01 22:59:36 +0100

    set x axis to be numerical

--- src/ts/charts/classic-jump-chart.ts
+++ src/ts/charts/classic-jump-chart.ts
@@ -1,27 +1,57 @@
-import Chart, { ChartConfiguration } from 'chart.js/auto'
+import Chart, { ChartConfiguration, ChartOptions } from "chart.js/auto";
 
-(async function() {
-    const labels = [1, 2, 3, 4, 5, 6, 7];
-        const data = {
-        labels: labels,
-        datasets: [{
-            label: 'My First Dataset',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1
-        }]
-    };
+(async function () {
+  const data1: { x: number; y: number }[] = [];
 
-    const config: ChartConfiguration = {
-        type: 'line',
-        data: data,
-    }
+  const data2 = [
+    { x: 1, y: 65 },
+    { x: 2, y: 59 },
+    { x: 3, y: 80 },
+    { x: 7, y: 81 },
+    { x: 9, y: 56 },
+    { x: 10, y: 55 },
+    { x: 18, y: 40 },
+  ];
 
-    const chartElement: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById('classic-jump-chart')!;
-  
-    new Chart(
-      chartElement, config
-    );
-  })();
-   
+  for (let i = 0; i < 2 * Math.PI; i += 0.05) {
+    data1.push({
+      x: i,
+      y: Math.sin(i),
+    });
+  }
+
+  const data = {
+    datasets: [
+      {
+        label: "My First Dataset",
+        fill: false,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0,
+        data: data1,
+      },
+    ],
+  };
+
+  const options: ChartOptions = {
+    scales: {
+      x: { type: "linear" },
+    },
+    elements: {
+      point: {
+        radius: 0,
+      },
+    },
+  };
+
+  const config: ChartConfiguration = {
+    type: "line",
+    data: data,
+    options: options,
+  };
+
+  const chartElement: HTMLCanvasElement = <HTMLCanvasElement>(
+    document.getElementById("classic-jump-chart")!
+  );
+
+  new Chart(chartElement, config);
+})();
