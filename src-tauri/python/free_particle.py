5929f179d0134dc6ba9734df26831c80f86e8bc2
xAdiro <adiro.rostek@gmail.com>
2024-01-07 06:09:21 +0100

    added divs for all charts

--- src-tauri/python/free_particle.py
+++ src-tauri/python/free_particle.py
@@ -4,10 +4,12 @@ from json_export import to_json
 from sys import argv
 
 
-def free_particle(x0, x1, dx=0.01):
+def free_particle(x0, x1, m, E, h_, dx=0.01):
+
     x = arange(x0, x1, dx)
     # A^2 * (x1-x0) = 1
     A = sqrt(1/abs(x1-x0))
+    k = 1/h_*sqrt(2*m*E)
     re = [A*cos(xn) for xn in x]
     im = [A*sin(xn) for xn in x]
 
@@ -18,4 +20,5 @@ def free_particle(x0, x1, dx=0.01):
     )
 
 
-print(free_particle(float(argv[1]), float(argv[2])))
+print(free_particle(float(argv[1]), float(argv[2]), float(
+    argv[3]), float(argv[4]), float(argv[5])))
