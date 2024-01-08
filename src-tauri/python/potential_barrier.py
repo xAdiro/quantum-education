e6883941ecce7c44fed35908772e35f3ec28b3ff
xAdiro <adiro.rostek@gmail.com>
2024-01-08 03:08:30 +0100

    changed potential barrier, still not working

--- src-tauri/python/potential_barrier.py
+++ src-tauri/python/potential_barrier.py
@@ -19,9 +19,9 @@ def _psi(x, a, E, V0, m, h_):
     k2 = 1/h_*sqrt(complex(2*m*(E-V0)))
 
     T = (4*k1*k2*e**(1j*k2*a))/((k1+k2)*(k2+k3)-(k3-k2)*(k1-k2)*e**(2j*k2*a))
-    C = (T * k3*(k1-k2)/(k2*(k1+k2)) * e**(1j*a*(k1+k2))) / \
-        (1 - e**(1j*a*(k1+k2)) * (k2-k1)/(k1+k2))
-    D = C*(k1+k2)/(k2-k1)
+    D = ((k1+k2)*k3*T*e**(1j*k3*a) - 2*k1*k2*e**(1j*k1*a)) / \
+        (k2*((k2-k1)*e**(1j*k1*a) - (k1+k2)*e**(-1j*k2*a)))
+    C = (2*k1+D*(k2-k1))/(k1+k2)
     R = C+D-1
 
     if x < 0:
@@ -37,7 +37,6 @@ def potential_barrier_test():
     y = [_psi(x0, 1, 1, 3, 1, 1) for x0 in x]
 
     import matplotlib.pyplot as plt
-
     plt.plot(x, [y0.real for y0 in y])
     plt.plot(x, [y0.imag for y0 in y])
     plt.show()
